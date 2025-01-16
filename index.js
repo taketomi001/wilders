5f754b87f6a8443edc99892b39b395c9d3ec225f
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-01-16 11:23:25 +0100

    change

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ import chalk from "chalk";
 const names = ["yahya", "teddy", "melissa", "oceane"];
 
 function tab(array) {
-  let yahia = [];
-  let teddy = [];
-  let melissa = [];
-  let oceane = [];
+for(let i =0; i<1; i++){
+console.log(chalk.blue(array[0]),chalk.red(array[1]),chalk.green(array[2]),chalk.yellow(array[3]));
+}
 
-  yahia = console.log(chalk.blue(array[0]));
-  teddy = console.log(chalk.red(array[1]));
-  melissa = console.log(chalk.green(array[2]));
-  oceane = console.log(chalk.yellow(array[3]));
-
-  return yahia, teddy, melissa, oceane;
 }
 
 // console.log(tab(names));
-tab(names)
+
+tab(names);
+
